9775d2cbae33d935de89c5f18745ad89293129f2
Dhaqane-00 <Abdilaahimowliid@gmail.com>
2024-11-17 22:32:50 +0300

    new

--- Backend/src/routes/auth.js
+++ Backend/src/routes/auth.js
@@ -11,6 +11,7 @@ router.get('/google',
 router.get('/google/callback',
   passport.authenticate('google', { session: false }),
   (req, res) => {
+    console.log(req.user);
     const { token, user } = req.user;
 
     // Set cookies with token and user info
