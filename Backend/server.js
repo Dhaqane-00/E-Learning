ac0345b2e80d6908074aa52c83b4d98f308cdb19
Dhaqane-00 <Abdilaahimowliid@gmail.com>
2024-11-17 13:15:39 +0300

    new

--- Backend/server.js
+++ Backend/server.js
@@ -13,6 +13,7 @@ const passportRouter = require('./src/routes/auth');
 const passport = require('passport');
 require('./src/config/passport-google');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 app.use(cookieParser());
 app.use(cors());
@@ -26,7 +27,13 @@ app.use(morgan('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
-app.use(express.static('public/uploads'));
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use("/public/uploads", express.static("public/uploads"));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
