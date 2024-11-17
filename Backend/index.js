7769b1d2882e0a43b4998a4dfd2601be03b462ef
Dhaqane-00 <Abdilaahimowliid@gmail.com>
2024-11-17 23:02:09 +0300

    new

--- Backend/index.js
+++ Backend/index.js
@@ -26,7 +26,6 @@ const corsOptions = {
     'https://accounts.google.com',
     'https://play.google.com'
   ],
-  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Cookie'],
   exposedHeaders: ['Set-Cookie'],
