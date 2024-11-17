1cbf3e4c4ddc88ddec37e8691aaaf6355a2354e2
Dhaqane-00 <Abdilaahimowliid@gmail.com>
2024-11-17 18:27:35 +0300

    n

--- Backend/src/routes/auth.js
+++ Backend/src/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+require('dotenv').config();
 
 // Google Auth Routes
 router.get('/google',
